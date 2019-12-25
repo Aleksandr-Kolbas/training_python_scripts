8eedc7ad20e679bb824461032b84cb93a448fc95
Aleksandr Kolbas <kolbas87@inbox.ru>
2019-12-26 01:15:27 +0300

    Little changes in api_number.py

--- api_number.py
+++ api_number.py
@@ -1,10 +1,9 @@
 import requests
 
 
-def get_math_fact_from_api():
+def get_math_fact_from_api(number):
     """Shows an interesting math fact (if any) about the requested number
-    from the API of the website http://numbersapi.com/"""
-    number = int(input('Enter the number you want to know about: '))
+    from the API of the website http://numbersapi.com/"""    
     api_url = "http://numbersapi.com/{}/math?json=true".format(number)
     res_from_api = requests.get(api_url)
     data_in_json = res_from_api.json()
@@ -14,4 +13,4 @@ def get_math_fact_from_api():
         print('Boring number. Nothing interesting.')
 
 
-get_math_fact_from_api()
+get_math_fact_from_api(int(input('Enter the number you want to know about: ')))
